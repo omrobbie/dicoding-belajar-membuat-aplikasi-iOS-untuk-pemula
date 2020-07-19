578c18015703256bb96601245a75bacc0e368974
omrobbie <putu.roby@yahoo.com>
2020-07-19 18:28:59 +0800

    test dummy data to the list

--- WoW/Controller/HomeVC.swift
+++ WoW/Controller/HomeVC.swift
@@ -15,12 +15,17 @@ class HomeVC: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return wonders.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        cell.textLabel?.text = "Item \(indexPath.row)"
+        let item = wonders[indexPath.row]
+        cell.textLabel?.text = item.title
         return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
