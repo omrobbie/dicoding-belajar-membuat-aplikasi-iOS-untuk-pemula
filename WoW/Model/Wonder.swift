6f3bec7a6005d0b3c7ad8ad3234bd793bbff48ad
omrobbie <putu.roby@yahoo.com>
2020-07-19 18:27:38 +0800

    create data model and dummy data

---
+++ WoW/Model/Wonder.swift
@@ -0,0 +1,36 @@
+//
+//  Wonder.swift
+//  WoW
+//
+//  Created by omrobbie on 19/07/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+struct Wonder {
+
+    let title: String
+    let description: String
+    let imageName: String
+}
+
+let wonders: [Wonder] = [
+    Wonder(title: "Pyramids of Giza", description: "The Giza necropolis, situated in the immediate vicinity of the southwestern suburbs of Cairo is probably the most famous ancient site in the world. The pyramids in Giza were built over the span of three generations – by Khufu, his second reigning son Khafre, and Menkaure. The Great Pyramid of Khufu is the oldest and sole remnant of the Seven Wonders of the Ancient World. Over 2 million blocks of stone were used to construct the pyramid, during a 20 year period concluding around 2560 BC. The pyramid is an awe-inspiring 139 meters (455 feet) high making it the largest pyramid in Egypt, although nearby Khafre’s Pyramid appears to be larger as it is build at a higher elevation.", imageName: "wow1"),
+
+    Wonder(title: "Great Wall of China", description: "The Great Wall of China built, rebuilt, and maintained between the 5th century BC and the 16th century to protect the northern borders of the Chinese Empire from the attacks of the Xiongnu tribes. Several walls have been built that were referred to as the Great Wall. One of the most famous is the wall built between 220–206 BC by the first Emperor of China but little of that wall remains. The majority of the existing wall were built during the Ming Dynasty (1368-1644 AD). The most comprehensive archaeological survey has recently concluded that the entire Great Wall, with all of its branches, stretches for 8,851.8 kilometers (5,500.3 miles).", imageName: "wow2"),
+
+    Wonder(title: "Machu Picchu", description: "One of the most beautiful and impressive ancient sites in the world, Machu Pichu was rediscovered in 1911 by Hawaiian historian Hiram after it lay hidden for centuries above the Urubamba Valley. The “Lost City of the Incas” is invisible from below and completely self-contained, surrounded by agricultural terraces and watered by natural springs. Although known locally, it was largely unknown to the outside world before being rediscovered in 1911. Since then, Machu Picchu has become the most important tourist attraction in Peru.", imageName: "wow3"),
+
+    Wonder(title: "Petra", description: "Petra, the fabled “rose red city, half as old as time”, was the ancient capital of the Nabataean kingdom. It is without a doubt Jordan’s most valuable treasure and greatest tourist attraction. A vast, unique city, carved into the side of the Wadi Musa Canyon centuries ago by the Nabataeans, who turned it into an important junction for the silk and spice routes that linked China, India and southern Arabia with Egypt, Greece and Rome. The most elaborate building in Petra is Al Khazneh (“The Treasury”‎), carved out of a sandstone rock face, it’s massive façade dwarfing everything around it.", imageName: "wow4"),
+
+    Wonder(title: "Teotihuacan", description: "In the 2nd century BC a new civilization arose in the valley of Mexico. This civilization built the flourishing metropolis of Teotihuacán and it’s huge step pyramids. The Pyramid of the Sun was built around 100 AD and is 75 meters (246 ft (75) high making it the largest building in Teotihuacán and one of the largest in Mesoamerica. The construction of the smaller Pyramid of the Moon started a century later and was finished in 450 A.D. Seven centuries after the demise of the Teotihuacán empire the pyramids were honored and utilized by the Aztecs and became a place of pilgrimage.", imageName: "wow5"),
+
+    Wonder(title: "Angkor", description: "Angkor Wat is a vast temple complex featuring the magnificent remains of several capitals of the Khmer Empire, from the 9th to the 15th century AD. These include the famous Angkor Wat temple, the world’s largest single religious monument, and the Bayon temple (at Angkor Thom) with its multitude of massive stone faces. During it’s long history Angkor went through many changes in religion converting between Hinduism to Buddhism several times. It has become a symbol of Cambodia, appearing on its national flag, and it is the country’s prime attraction for visitors.", imageName: "wow6"),
+
+    Wonder(title: "Colosseum", description: "The Colosseum is the largest and most famous amphitheater ever built in the Roman Empire. Its construction was started by emperor Vespasian of the Flavian dynasty in 72 AD and was finished by his son Titus in 80 AD. During the Colosseum’s opening ceremonies, spectacles were held for 100 days in which 5,000 of animals and 2,000 gladiators were killed. The Colosseum was capable of holding some 50,000 spectators who could enter the building through no less than 80 entrances. Spectators were protected from the rain and heat of the sun by sail,s called the “velarium”, around the top of the attic. It is one of Rome’s most popular tourist attractions and has become an iconic symbol of Imperial Rome.", imageName: "wow7"),
+
+    Wonder(title: "Taj Mahal", description: "The Taj Mahal is an immense mausoleum of white marble, built between 1632 and 1653 by order of the Mughal emperor Shah Jahan in memory of his favorite wife. The Taj is one of the most well preserved and architecturally beautiful tombs in the world, one of the masterpieces of Mughal architecture, and one of the great sites of the world’s heritage. Called “a teardrop on the cheek of eternity”, the monument is actually an integrated complex of structures. Besides the white domed marble mausoleum it includes several other beautiful buildings, reflecting pools, and extensive ornamental gardens with flowering trees and bushes.", imageName: "wow8"),
+
+    Wonder(title: "Easter Island", description: "The world famous moai are monolithic statues located on Easter Island, one of the most isolated islands on Earth. The statues were carved by the Polynesian colonizers of the island, mostly between circa 1250 AD and 1500 AD. In addition to representing deceased ancestors, the moai were also regarded as the embodiment of powerful living or former chiefs. The tallest moai erected, called Paro, was almost 10 meters (33 ft) high and weighed 75 tonnes. The statues were till standing when Europeans first visited the island, but most would be cast down during later conflicts between clans. Today about 50 moai have been re-erected on Easter Island or museums elsewhere.", imageName: "wow9"),
+
+    Wonder(title: "Parthenon", description: "The Parthenon on top of the Acropolis is one of the most famous buildings in the world and a visit to Athens is not complete without visiting this temple. The construction of the Parthenon started in in 447 BC, replacing and older temple that was destroyed by the Persians, and completed in 432 BC. The purpose of the Parthenon was to house a massive statue of Athena Parthenos made from ivory, silver and gold. Sometime in the 5th century the statue was looted by one of the Roman Emperors, and taken to Constantinople, where it was later destroyed. During its long life the Parthenon has also served as a fortress, a church, a mosque and as a powder magazine.", imageName: "wow10")
+]
